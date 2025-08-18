f921e057cbc33cd767dfa35455c21dbf6315b4d7
Douglas Moura <d.moura250304@gmail.com>
2025-08-18 00:38:12 -0500

    Feat: adição de documentos estáticos.

---
+++ plataforma_projetos/static/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menu-btn");
+  const sidebar = document.getElementById("sidebar");
+
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+});
