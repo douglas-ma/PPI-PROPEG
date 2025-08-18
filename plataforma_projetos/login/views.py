ec5feec758b88d7a098b010046d406bf3b2b0560
Douglas Moura <d.moura250304@gmail.com>
2025-08-18 00:37:31 -0500

    Feat: adição do cabeçalho da aplicação

--- plataforma_projetos/login/views.py
+++ plataforma_projetos/login/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'login/login.html')
+    return render(request, 'home/home.html')
