b5fdeb021d03747a92957ab01502d593979ddf10
Douglas Moura <d.moura250304@gmail.com>
2025-09-04 17:18:05 -0500

    Feat: registro das entidades através do admin superuser

--- plataforma_projetos/projetos_institucionais/admin.py
+++ plataforma_projetos/projetos_institucionais/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from .models import (
+    Usuario, Endereco, Titulacao, CentroLotacao, CursoGraduacao, 
+    ProgramaPos, TipoEtico, ODS, GrupoPesquisa, AgenciaFinanciadora,
+    Projeto, Documento, Ata, Relatorio
+)
 
-from .models import CentroLotacao
-
+admin.site.register(Usuario)
+admin.site.register(Endereco)
+admin.site.register(Titulacao)
 admin.site.register(CentroLotacao)
+admin.site.register(CursoGraduacao)
+admin.site.register(ProgramaPos)
+admin.site.register(TipoEtico)
+admin.site.register(ODS)
+admin.site.register(GrupoPesquisa)
+admin.site.register(AgenciaFinanciadora)
+admin.site.register(Projeto)
+admin.site.register(Documento)
+admin.site.register(Ata)
+admin.site.register(Relatorio)
