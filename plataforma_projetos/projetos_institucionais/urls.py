00c59b219b9f606eab1d42c36a563a5f7b5d50ec
Douglas Moura <d.moura250304@gmail.com>
2025-09-04 17:20:57 -0500

    Refatoração dentro das urls

--- plataforma_projetos/projetos_institucionais/urls.py
+++ plataforma_projetos/projetos_institucionais/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-
-router = DefaultRouter()
-router.register(r'projetos', views.ProjetoViewSet, basename='projeto')
-router.register(r'centros-lotacao', views.CentroLotacaoViewSet, basename='centrolotacao')
-router.register(r'ods', views.OdsViewSet, basename='ods')
-router.register(r'relatorios', views.RelatorioViewSet, basename='relatorio')
-router.register(r'alunos', views.AlunoViewSet, basename='aluno')
-# Registre as outras ViewSets aqui
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('', include(router.urls)),
     path('', views.request_home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
