3a3b8af12aa2ee4c99c7067917741ee0b381499e
Douglas Moura <d.moura250304@gmail.com>
2025-09-04 19:01:08 -0500

    Feat: Criação de novo template base para o sistema interno.

--- plataforma_projetos/static/js/scripts.js
+++ plataforma_projetos/static/js/scripts.js
@@ -1,8 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.getElementById("menu-btn");
-  const sidebar = document.getElementById("sidebar");
+// const sidebar = document.getElementById('sidebar');
+// const toggleBtn = document.getElementById('toggle-btn');
 
-  menuBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-  });
+// toggleBtn.addEventListener('click', () => {
+//     sidebar.classList.toggle('collapsed');
+// });
+
+// Garante que o script rode após o carregamento da página
+document.addEventListener('DOMContentLoaded', function() {
+
+    const sidebar = document.getElementById('sidebar');
+    const toggleBtn = document.getElementById('toggle-btn');
+
+    // Verifica se os elementos existem antes de adicionar o evento
+    if (sidebar && toggleBtn) {
+        toggleBtn.addEventListener('click', function (event) {
+            // Previne o comportamento padrão do link (que é navegar para '#')
+            event.preventDefault();
+            
+            // Adiciona ou remove a classe 'collapsed' da sidebar
+            sidebar.classList.toggle('collapsed');
+        });
+    }
+
 });
