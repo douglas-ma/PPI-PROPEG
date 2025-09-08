b5607b692bd197d5a63baf9c244e831bb9b48933
Douglas Moura <d.moura250304@gmail.com>
2025-09-07 21:18:21 -0500

    Feat: Refatoração dos estilos para as telas de home, login e registrar e functions para mudança de caracteres na senha e cpf na inserção dos campos.

--- plataforma_projetos/static/js/scripts.js
+++ plataforma_projetos/static/js/scripts.js
@@ -1,10 +1,3 @@
-// const sidebar = document.getElementById('sidebar');
-// const toggleBtn = document.getElementById('toggle-btn');
-
-// toggleBtn.addEventListener('click', () => {
-//     sidebar.classList.toggle('collapsed');
-// });
-
 // Garante que o script rode após o carregamento da página
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -19,6 +12,51 @@ document.addEventListener('DOMContentLoaded', function() {
             
             // Adiciona ou remove a classe 'collapsed' da sidebar
             sidebar.classList.toggle('collapsed');
+        });
+    }
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    function setupPasswordToggle(inputId, buttonId) {
+        const passwordInput = document.getElementById(inputId);
+        const toggleButton = document.getElementById(buttonId);
+
+        if (passwordInput && toggleButton) {
+            const eyeIcon = toggleButton.querySelector("i");
+            toggleButton.addEventListener("click", function () {
+                const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+                passwordInput.setAttribute("type", type);
+                
+                if (type === "password") {
+                    eyeIcon.classList.remove("bi-eye-slash-fill");
+                    eyeIcon.classList.add("bi-eye-fill");
+                } else {
+                    eyeIcon.classList.remove("bi-eye-fill");
+                    eyeIcon.classList.add("bi-eye-slash-fill");
+                }
+            });
+        }
+    }
+
+    setupPasswordToggle('senha', 'toggleSenha');
+    setupPasswordToggle('id_password', 'toggleSenha');
+    setupPasswordToggle('id_password2', 'toggleSenha2');
+
+
+    const cpfInputLogin = document.getElementById("cpf");
+    const cpfInputRegistro = document.getElementById("id_cpf");
+    const cpfInput = cpfInputLogin || cpfInputRegistro;
+
+    if (cpfInput) {
+        cpfInput.addEventListener("input", function (e) {
+            let value = cpfInput.value.replace(/\D/g, "");
+            value = value.substring(0, 11);
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d)/, "$1.$2");
+            value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+            cpfInput.value = value;
         });
     }
 
