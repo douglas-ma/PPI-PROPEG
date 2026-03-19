5f2c98c1d6e627811afc9890a98dab43acb81b3f
Douglas Moura <d.moura250304@gmail.com>
2026-03-19 18:21:21 -0500

    Fix: corrige caminho de criação de projeto

--- plataforma_projetos/projetos_institucionais/urls.py
+++ plataforma_projetos/projetos_institucionais/urls.py
@@ -64,6 +64,7 @@ urlpatterns = [
     path('ajuda/', views.ajuda, name='ajuda'),
     path('projetos/<int:pk>/certificado/<int:membro_pk>/', views.gerar_certificado, name='gerar_certificado'),
     path('projetos/<int:pk>/certificados/', views.listar_certificados, name='listar_certificados'),
+    path('ajax/load-cursos/', views.load_cursos, name='ajax_load_cursos'),
 ]   
 
 if settings.DEBUG:
