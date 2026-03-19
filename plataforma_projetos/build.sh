2f11726c0b81dee85a2b9ca5152018d49e43f42e
Douglas Moura <d.moura250304@gmail.com>
2026-03-18 21:31:22 -0500

    Deu certo

--- plataforma_projetos/build.sh
+++ plataforma_projetos/build.sh
@@ -11,24 +11,6 @@ python manage.py collectstatic --no-input
 # Aplica as migrations
 python manage.py migrate
 
-# Testa o email
-python manage.py shell -c "
-from django.core.mail import send_mail
-from django.conf import settings
-print('Tentando enviar para:', settings.EMAIL_HOST_USER)
-try:
-    send_mail(
-        'Teste PROPEG',
-        'Se recebeu este email, o envio está funcionando.',
-        settings.EMAIL_HOST_USER,
-        [settings.EMAIL_HOST_USER],
-        fail_silently=False,
-    )
-    print('SUCESSO: email enviado')
-except Exception as e:
-    print('ERRO:', type(e).__name__, str(e))
-"
-
 # Adicione o super user se ele não existir
 python manage.py shell -c "
 from projetos_institucionais.models import Usuario
